ed1bf6c6a1dd7d60eb53d7517a1caec6a841e508
Beckelund <enjohan@hotmail.com>
2022-12-21 16:58:21 +0100

    render cube

--- webgl-demo.js
+++ webgl-demo.js
@@ -6,6 +6,12 @@ main();
 //
 // start here
 //
+
+//Variables time
+let squareRotation = 0.0;
+let cubeRotation = 0.0;
+let deltaTime = 0;
+
 function main() {
     const canvas = document.querySelector("#glcanvas");
     // Initialize the GL context
@@ -19,34 +25,61 @@ function main() {
     return;
     }
 
+    console.log(gl.drawingBufferWidth);
+    console.log(gl.canvas.clientWidth);
+    console.log(gl.drawingBufferHeight);
+    console.log(gl.canvas.clientHeight);
 
     // Vertex shader program
     // Vertex shader program
 
     const vsSource = `
     attribute vec4 aVertexPosition;
-    attribute vec4 aVertexColor;
+    attribute vec3 aVertexNormal;
+    attribute vec2 aTextureCoord;
 
+    uniform mat4 uNormalMatrix;
     uniform mat4 uModelViewMatrix;
     uniform mat4 uProjectionMatrix;
 
-    varying lowp vec4 vColor;
+    varying highp vec2 vTextureCoord;
+    varying highp vec3 vLighting;
 
     void main(void) {
-    gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-    vColor = aVertexColor;
+      gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+      vTextureCoord = aTextureCoord;
+
+      // Apply lighting effect
+
+      highp vec3 ambientLight = vec3(0.3, 0.3, 0.3);
+      highp vec3 directionalLightColor = vec3(1, 1, 1);
+      highp vec3 directionalVector = normalize(vec3(0.85, 0.8, 0.0));
+
+      highp vec4 transformedNormal = uNormalMatrix * vec4(aVertexNormal, 1.0);
+
+      highp float directional = max(dot(transformedNormal.xyz, directionalVector), 0.0);
+      vLighting = ambientLight + (directionalLightColor * directional);
     }
-    `;
+  `;
+
+
 
 
     // Fragment shader program
     const fsSource = `
-    varying lowp vec4 vColor;
+    varying highp vec2 vTextureCoord;
+    varying highp vec3 vLighting;
+
+    uniform sampler2D uSampler;
 
     void main(void) {
-    gl_FragColor = vColor;
+      highp vec4 texelColor = texture2D(uSampler, vTextureCoord);
+
+      gl_FragColor = vec4(texelColor.rgb * vLighting, texelColor.a);
     }
-    `;
+  `;
+
+
 
     // Initialize a shader program; this is where all the lighting
     // for the vertices and so forth is established.
@@ -59,18 +92,23 @@ function main() {
     const programInfo = {
         program: shaderProgram,
         attribLocations: {
-        vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
-        vertexColor: gl.getAttribLocation(shaderProgram, "aVertexColor"),
+          vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
+          vertexNormal: gl.getAttribLocation(shaderProgram, "aVertexNormal"),
+          textureCoord: gl.getAttribLocation(shaderProgram, "aTextureCoord"),
         },
         uniformLocations: {
-        projectionMatrix: gl.getUniformLocation(shaderProgram, "uProjectionMatrix"),
-        modelViewMatrix: gl.getUniformLocation(shaderProgram, "uModelViewMatrix"),
+          projectionMatrix: gl.getUniformLocation(shaderProgram, "uProjectionMatrix"),
+          modelViewMatrix: gl.getUniformLocation(shaderProgram, "uModelViewMatrix"),
+          normalMatrix: gl.getUniformLocation(shaderProgram, "uNormalMatrix"),
+          uSampler: gl.getUniformLocation(shaderProgram, "uSampler"),
         },
-    };
+      };
+      
+      
   
 
     // Set clear color to black, fully opaque
-    gl.clearColor(0.0, 0.8, 0.0, 1.0);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
     // Clear the color buffer with specified clear color
     gl.clear(gl.COLOR_BUFFER_BIT);
 
@@ -78,8 +116,27 @@ function main() {
     // objects we'll be drawing.
     const buffers = initBuffers(gl);
 
+    // Load texture
+    const texture = loadTexture(gl, "cubetexture.png");
+    // Flip image pixels into the bottom-to-top order that WebGL expects.
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+
     // Draw the scene
-    drawScene(gl, programInfo, buffers);
+    let then = 0;
+
+    // Draw the scene repeatedly
+    function render(now) {
+        now *= 0.001; // convert to seconds
+        deltaTime = now - then;
+        then = now;
+
+        drawScene(gl, programInfo, buffers, texture, cubeRotation);
+        cubeRotation += deltaTime;
+
+
+        requestAnimationFrame(render);
+    }
+    requestAnimationFrame(render);
 
 }
 
@@ -137,5 +194,77 @@ function initShaderProgram(gl, vsSource, fsSource) {
     }
 
     return shader;
-    }
+}
+
+//
+// Initialize a texture and load an image.
+// When the image finished loading copy it into the texture.
+//
+function loadTexture(gl, url) {
+    const texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+  
+    // Because images have to be downloaded over the internet
+    // they might take a moment until they are ready.
+    // Until then put a single pixel in the texture so we can
+    // use it immediately. When the image has finished downloading
+    // we'll update the texture with the contents of the image.
+    const level = 0;
+    const internalFormat = gl.RGBA;
+    const width = 1;
+    const height = 1;
+    const border = 0;
+    const srcFormat = gl.RGBA;
+    const srcType = gl.UNSIGNED_BYTE;
+    const pixel = new Uint8Array([0, 0, 255, 255]); // opaque blue
+    gl.texImage2D(
+      gl.TEXTURE_2D,
+      level,
+      internalFormat,
+      width,
+      height,
+      border,
+      srcFormat,
+      srcType,
+      pixel
+    );
+  
+    const image = new Image();
+    image.onload = () => {
+      gl.bindTexture(gl.TEXTURE_2D, texture);
+      gl.texImage2D(
+        gl.TEXTURE_2D,
+        level,
+        internalFormat,
+        srcFormat,
+        srcType,
+        image
+      );
+  
+      // WebGL1 has different requirements for power of 2 images
+      // vs non power of 2 images so check if the image is a
+      // power of 2 in both dimensions.
+      if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
+        // Yes, it's a power of 2. Generate mips.
+        gl.generateMipmap(gl.TEXTURE_2D);
+      } else {
+        // No, it's not a power of 2. Turn off mips and set
+        // wrapping to clamp to edge
+        // gl.NEAREST is also allowed, instead of gl.LINEAR, as neither mipmap.
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+        // Prevents s-coordinate wrapping (repeating).
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        // Prevents t-coordinate wrapping (repeating).
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+      }
+    };
+    image.src = url;
+  
+    return texture;
+  }
+  
+  function isPowerOf2(value) {
+    return (value & (value - 1)) === 0;
+  }
+  
   
