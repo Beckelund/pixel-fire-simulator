30caa370f3cf697952d73e25d3c7f10c1ed953d5
Beckelund <enjohan@hotmail.com>
2023-01-10 12:17:34 +0100

    util functions folder

---
+++ util/setPixel.js
@@ -0,0 +1,19 @@
+function setPixel(image, x, y, r, g, b, a)
+{
+    //const width = 640;
+    //const height = 480;
+
+    let currentPixel = (x + y * image.width) * 4;
+
+    //return if out of bounds
+    if(x < 0 || x >= image.width || y < 0 || y > image.height) return;
+
+
+
+    image.data[currentPixel + 0] = r;
+    image.data[currentPixel + 1] = g;
+    image.data[currentPixel + 2] = b;
+    image.data[currentPixel + 3] = a;
+}
+
+export { setPixel }
